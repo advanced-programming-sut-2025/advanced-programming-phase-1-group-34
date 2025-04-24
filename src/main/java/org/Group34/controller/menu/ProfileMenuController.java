8ea90fc825a6b221d4b105b78a41c7dc24d95cf4
Fatemeh Damirchi <fa.damirchy@gmail.com>
2025-04-24 18:00:26 +0330

    refactor: reversed the classes

--- src/main/java/org/Group34/controller/menu/ProfileMenuController.java
+++ src/main/java/org/Group34/controller/menu/ProfileMenuController.java
@@ -1,45 +1,5 @@
 package main.java.org.Group34.controller.menu;
 
-import main.java.org.Group34.model.enums.Menu;
-import main.java.org.Group34.model.enums.command.menu.ProfileMenuCommand;
-import main.java.org.Group34.view.menu.AppMenu;
+public class ProfileMenuController {
 
-import java.util.Scanner;
-
-public class ProfileMenuController extends AppMenu {
-    private ProfileMenuController controller;
-
-    public ProfileMenuController() {
-        currentMenu = Menu.PROFILE_MENU;
-        controller = new ProfileMenuController();
-    }
-
-    @Override
-    public void run(Scanner scanner) {
-        while (currentMenu == Menu.PROFILE_MENU) {
-            String command = scanner.nextLine().trim();
-
-            if (command.matches(ProfileMenuCommand.CHANGE_USERNAME.getRegex())) {
-
-            }
-            else if (command.matches(ProfileMenuCommand.CHANGE_PASSWORD.getRegex())) {
-
-            }
-            else if (command.matches(ProfileMenuCommand.CHANGE_NICKNAME.getRegex())) {
-
-            }
-            else if (command.matches(ProfileMenuCommand.CHANGE_EMAIL.getRegex())) {
-
-            }
-            else if (command.matches(ProfileMenuCommand.SHOW_INFO.getRegex())) {
-
-            }
-            else if (command.matches(ProfileMenuCommand.SHOW_MENU.getRegex())) {
-                showMessage("You are currently in Profile Menu.");
-            }
-            else {
-                showMessage("Invalid command.");
-            }
-        }
-    }
 }
--- src/main/java/org/Group34/view/menu/ProfileMenu.java
+++ src/main/java/org/Group34/view/menu/ProfileMenu.java
@@ -1,4 +1,45 @@
 package main.java.org.Group34.view.menu;
 
-public class ProfileMenu {
+import main.java.org.Group34.controller.menu.ProfileMenuController;
+import main.java.org.Group34.model.enums.Menu;
+import main.java.org.Group34.model.enums.command.menu.ProfileMenuCommand;
+
+import java.util.Scanner;
+
+public class ProfileMenu extends AppMenu {
+    private ProfileMenuController controller;
+
+    public ProfileMenu() {
+        currentMenu = Menu.PROFILE_MENU;
+        controller = new ProfileMenuController();
+    }
+
+    @Override
+    public void run(Scanner scanner) {
+        while (currentMenu == Menu.PROFILE_MENU) {
+            String command = scanner.nextLine().trim();
+
+            if (command.matches(ProfileMenuCommand.CHANGE_USERNAME.getRegex())) {
+
+            }
+            else if (command.matches(ProfileMenuCommand.CHANGE_PASSWORD.getRegex())) {
+
+            }
+            else if (command.matches(ProfileMenuCommand.CHANGE_NICKNAME.getRegex())) {
+
+            }
+            else if (command.matches(ProfileMenuCommand.CHANGE_EMAIL.getRegex())) {
+
+            }
+            else if (command.matches(ProfileMenuCommand.SHOW_INFO.getRegex())) {
+
+            }
+            else if (command.matches(ProfileMenuCommand.SHOW_MENU.getRegex())) {
+                showMessage("You are currently in Profile Menu.");
+            }
+            else {
+                showMessage("Invalid command.");
+            }
+        }
+    }
 }
