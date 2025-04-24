91616512ffb8b5385667cf8d4b1a355cb1d9fca1
Fatemeh Damirchi <fa.damirchy@gmail.com>
2025-04-24 17:54:32 +0330

    coded the base structure of App

--- src/main/java/org/Group34/model/App.java
+++ src/main/java/org/Group34/model/App.java
@@ -1,4 +1,26 @@
 package main.java.org.Group34.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Holds the game data
+ * Only essential fields for Main menu and Profile menu are added
+ * Make sure to complete it
+ */
+
 public class App {
+    private List<User> users;
+
+    public App() {
+        users = new ArrayList<>();
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 }
