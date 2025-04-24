55bd0f7385ae0a182f25528a6f252d21abc3bbe9
Fatemeh Damirchi <fa.damirchy@gmail.com>
2025-04-24 17:39:39 +0330

    finished "MainMenu.java"

--- src/main/java/org/Group34/view/menu/MainMenu.java
+++ src/main/java/org/Group34/view/menu/MainMenu.java
@@ -1,22 +1,37 @@
 package main.java.org.Group34.view.menu;
 
-import main.java.org.Group34.controller.menu.MainMenuController;
 import main.java.org.Group34.model.enums.Menu;
+import main.java.org.Group34.model.enums.command.menu.MainMenuCommand;
 
 import java.util.Scanner;
 
-public class MainMenu extends AppMenu{
-    private final MainMenuController controller;
+/**
+ * This menu doesn't need controller
+ * All operations are about navigating to other menus
+ */
 
+public class MainMenu extends AppMenu{
     public MainMenu() {
         currentMenu = Menu.MAIN_MENU;
-        controller = new MainMenuController();
     }
 
     @Override
     public void run(Scanner scanner) {
         while (currentMenu == Menu.MAIN_MENU) {
             String command = scanner.nextLine().trim();
+
+            if(command.matches(MainMenuCommand.ENTER_PROFILE_MENU.getRegex())) {
+                setMenu(Menu.PROFILE_MENU);
+            }
+            else if (command.matches(MainMenuCommand.ENTER_GAME_MENU.getRegex())) {
+                setMenu(Menu.GAME_MENU);
+            }
+            else if (command.matches(MainMenuCommand.LOGOUT.getRegex())) {
+                setMenu(Menu.SIGNUP_MENU);
+            }
+            else if (command.matches(MainMenuCommand.SHOW_MENU.getRegex())) {
+                showMessage("You are currently in Main Menu.");
+            }
         }
     }
 }
