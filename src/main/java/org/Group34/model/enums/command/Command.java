7af980d1c64e0e4d7bab542bb98f724f9706544d
Fatemeh Damirchi <fa.damirchy@gmail.com>
2025-04-24 15:47:58 +0330

    minor update on "Command.java" Interface

--- src/main/java/org/Group34/model/enums/command/Command.java
+++ src/main/java/org/Group34/model/enums/command/Command.java
@@ -1,4 +1,16 @@
 package main.java.org.Group34.model.enums.command;
 
+/**
+ * Each command defines a regular expression pattern used to match user input
+ *
+ * This interface should be implemented by enums representing commands
+ */
+
 public interface Command {
+    /**
+     * Returns the regular expression pattern associated with the command.
+     *
+     * @return the regex pattern for matching user input
+     */
+    String getRegex();
 }
