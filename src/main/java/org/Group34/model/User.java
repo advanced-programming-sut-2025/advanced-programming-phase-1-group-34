aaa71d7d006bd45d9faaeecc44d2976d5430b022
Fatemeh Damirchi <fa.damirchy@gmail.com>
2025-04-24 17:52:15 +0330

    coded the base structure of User

--- src/main/java/org/Group34/model/User.java
+++ src/main/java/org/Group34/model/User.java
@@ -1,4 +1,46 @@
 package main.java.org.Group34.model;
 
+/**
+ * Represents a user in the game system
+ * Only essential fields for Main menu and Profile menu are added
+ * Make sure to complete it
+ */
+
 public class User {
+    private String username;
+    private String password;
+    private String email;
+    private String nickname;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 }
