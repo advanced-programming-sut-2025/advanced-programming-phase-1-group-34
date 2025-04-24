1b7e22b5c677cbd4be4ee6d80ce44552244e8c4f
Fatemeh Damirchi <fa.damirchy@gmail.com>
2025-04-24 16:40:55 +0330

    minor change

--- src/main/java/org/Group34/controller/menu/ProfileMenuController.java
+++ src/main/java/org/Group34/controller/menu/ProfileMenuController.java
@@ -1,4 +1,5 @@
 package main.java.org.Group34.controller.menu;
 
 public class ProfileMenuController {
+
 }
--- src/main/java/org/Group34/view/menu/MainMenu.java
+++ src/main/java/org/Group34/view/menu/MainMenu.java
@@ -1,4 +1,22 @@
 package main.java.org.Group34.view.menu;
 
-public class MainMenu {
+import main.java.org.Group34.controller.menu.MainMenuController;
+import main.java.org.Group34.model.enums.Menu;
+
+import java.util.Scanner;
+
+public class MainMenu extends AppMenu{
+    private final MainMenuController controller;
+
+    public MainMenu() {
+        currentMenu = Menu.MAIN_MENU;
+        controller = new MainMenuController();
+    }
+
+    @Override
+    public void run(Scanner scanner) {
+        while (currentMenu == Menu.MAIN_MENU) {
+            String command = scanner.nextLine().trim();
+        }
+    }
 }
