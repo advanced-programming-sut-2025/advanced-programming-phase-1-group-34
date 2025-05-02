92d658678c0f43abec934181dca34b0d06f9ced7
Fatemeh Damirchi <fa.damirchy@gmail.com>
2025-05-02 16:09:57 +0330

    connected menus

--- src/main/java/org/Group34/Main.java
+++ src/main/java/org/Group34/Main.java
@@ -1,16 +1,13 @@
 package org.Group34;
 
-import org.Group34.model.App;
-import org.Group34.model.enums.Menu;
+import org.Group34.view.AppView;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        while (App.getCurrentMenu() != Menu.EXIT_MENU){
-            App.getAppMenu().run(scanner);
-        }
+        AppView appView = new AppView();
+        appView.run(scanner);
     }
 }
