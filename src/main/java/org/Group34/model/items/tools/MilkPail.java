6c1771c56fb995ed6e4c09c360b3fd5c478c6645
Fatemeh Damirchi <fa.damirchy@gmail.com>
2025-05-11 10:01:54 +0330

    created milk pail tool

--- src/main/java/org/Group34/model/items/tools/MilkPail.java
+++ src/main/java/org/Group34/model/items/tools/MilkPail.java
@@ -1,9 +1,28 @@
 package org.Group34.model.items.tools;
 
+import org.Group34.model.enums.animals.AnimalType;
 import org.Group34.model.items.Item;
 
-public class MilkPail implements Item { // TODO This class must be filled.
+public class MilkPail implements Item {
+    private final int ENERGY_COST = 4;
+
     public String getName() {
         return "Milk Pail";
+    }
+
+    public int getEnergy() {
+        return ENERGY_COST;
+    }
+
+    public String getUsage() {
+        return "Used to milk animals.";
+    }
+
+    public String getAcquisition() {
+        return "Buy from Marnie's Ranch for 1000 gold.";
+    }
+
+    public boolean canMilk(AnimalType animalType) {
+        return animalType == AnimalType.COW || animalType == AnimalType.GOAT;
     }
 }
