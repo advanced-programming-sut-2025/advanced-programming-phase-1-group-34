93f874f412fd9ce14f42683a975079739ea57742
Fatemeh Damirchi <fa.damirchy@gmail.com>
2025-05-14 18:27:23 +0330

    update on npcs

---
+++ src/main/java/org/Group34/model/entities/npcs/NPCData.java
@@ -0,0 +1,15 @@
+package org.Group34.model.entities.npcs;
+
+import java.util.List;
+
+public class NPCData {
+    private List<NPC> npc;
+
+    public List<NPC> getNpc() {
+        return npc;
+    }
+
+    public void setNpc(List<NPC> npc) {
+        this.npc = npc;
+    }
+}
--- src/main/java/org/Group34/model/entities/npcs/quests/ActivationCondition.java
+++
@@ -1,7 +0,0 @@
-package org.Group34.model.entities.npcs.quests;
-
-import org.Group34.model.entities.Player;
-
-public interface ActivationCondition {
-    boolean canActivate(Player player);
-}
