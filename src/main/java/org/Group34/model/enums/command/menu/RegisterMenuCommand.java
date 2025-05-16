27c120e1961f03d6f69ecf805c75c84dddcda611
Fatemeh Damirchi <fa.damirchy@gmail.com>
2025-05-16 11:37:15 +0330

    added login menu test cases

--- src/main/java/org/Group34/model/enums/command/menu/RegisterMenuCommand.java
+++ src/main/java/org/Group34/model/enums/command/menu/RegisterMenuCommand.java
@@ -6,6 +6,7 @@ public enum RegisterMenuCommand implements Command {
     Register ("\\s*register\\s+-u\\s+(?<username>\\S+)\\s+-p\\s+(?<password>\\S+)\\s+(?<passwordConfirm>\\S+)\\s+-n\\s+(?<nickname>\\S+)\\s+-e\\s+(?<email>\\S+)\\s+-g\\s+(?<gender>\\S+)\\s*"),
     RegisterWithRandomPassword ("\\s*register\\s+-u\\s+(?<username>\\S+)\\s+-p\\s+random\\s+-n\\s+(?<nickname>\\S+)\\s+-e\\s+(?<email>\\S+)\\s+-g\\s+(?<gender>\\S+)\\s*"),
     PickQuestion ("\\s*pick\\s+question\\s+-q\\s+(?<questionNumber>[0-9]+)\\s+-a\\s+(?<answer>.+)\\s+-c\\s+(?<answerConfirm>.+)\\s*"),
+    GO_TO_LOGIN("\\s*go\\s+to\\s+login\\s*"),
 
     SHOW_MENU("show\\s+current\\s+menu"),
 
