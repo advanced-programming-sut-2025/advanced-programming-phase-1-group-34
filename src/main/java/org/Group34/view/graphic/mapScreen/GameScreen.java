26e1a4169a2c4d859464180b1d6bb824e1092f53
Fatemeh Damirchi <fa.damirchy@gmail.com>
2025-08-03 10:41:24 +0330

    minor change

--- src/main/java/org/Group34/view/graphic/mapScreen/GameScreen.java
+++ src/main/java/org/Group34/view/graphic/mapScreen/GameScreen.java
@@ -4,9 +4,11 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -32,15 +34,15 @@ import org.Group34.model.map.Space;
 import org.Group34.view.graphic.GameMenuGraphic;
 import org.Group34.view.graphic.ItemsGraphic;
 import org.Group34.view.graphic.dialogs.GreenhouseRepairDialog;
+import org.Group34.view.graphic.menuScreen.MainMenuScreen;
 
 public class GameScreen extends ScreenAdapter {
-    // Constants
     private static final int TILE_SIZE = 32;
     private static final int VIEWPORT_WIDTH = 30;
     private static final int VIEWPORT_HEIGHT = 15;
     private static final float MOVE_INTERVAL = 0.13f;
+    private static final float MESSAGE_DURATION = 3f;
 
-    // Main components
     private final Stage stage;
     private final Skin skin;
     private final Game game;
@@ -50,23 +52,19 @@ public class GameScreen extends ScreenAdapter {
     private final Map gameMap;
     private final SpriteBatch batch;
     private final OrthographicCamera camera;
-
-    // Sub-components
     private final UIManager uiManager;
     private final EnvironmentManager environmentManager;
     private MapRenderer mapRenderer;
     private final WeatherRenderer weatherRenderer;
-
-    // NPCs
     private boolean NPCsInitialized = false;
     private NPCDialogueManager npcDialogueManager;
-
-    // Game state
     private float moveTimer = 0;
-
-    // Other items
     private ItemsGraphic toolsGraphic;
     private GameMenuGraphic gameMenuGraphic;
+
+    private BitmapFont messageFont;
+    private String currentMessage = "";
+    private float messageTimer = 0;
 
     public GameScreen(Skin skin, Game game, MyGame myGame, GameController gameController) {
         this.skin = skin;
@@ -78,7 +76,6 @@ public class GameScreen extends ScreenAdapter {
         this.camera = new OrthographicCamera();
         this.stage = new Stage(new ScreenViewport());
 
-        // Initialize player
         User currentUser = App.getCurrentUser();
         if (currentUser != null) {
             this.player = myGame.players().get(currentUser);
@@ -86,20 +83,17 @@ public class GameScreen extends ScreenAdapter {
             this.player = myGame.players().values().iterator().next();
         }
 
-        // Initialize other items
         toolsGraphic = new ItemsGraphic(batch, player, gameController);
         gameMenuGraphic = new GameMenuGraphic(batch, player);
-
-        // Initialize sub-components
         this.uiManager = new UIManager(skin, game, stage);
         this.environmentManager = new EnvironmentManager(myGame, gameController);
         this.mapRenderer = new MapRenderer(gameMap, player, TILE_SIZE, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
         this.weatherRenderer = new WeatherRenderer();
-
-        // Initialize NPC dialogue manager
         this.npcDialogueManager = new NPCDialogueManager();
 
-        // Set up camera
+        messageFont = new BitmapFont();
+        messageFont.setColor(Color.WHITE);
+
         camera.setToOrtho(false, VIEWPORT_WIDTH * TILE_SIZE, VIEWPORT_HEIGHT * TILE_SIZE);
         Gdx.input.setInputProcessor(stage);
     }
@@ -110,11 +104,9 @@ public class GameScreen extends ScreenAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         updateCamera();
 
-        // Update environment and UI
         environmentManager.update();
         uiManager.update(environmentManager);
 
-        // Update NPCs dialogue system
         if (!NPCsInitialized) {
             Space currentSpace = gameMap.getCurrentPlayerFarm(player);
             environmentManager.initializeNPCs(currentSpace);
@@ -124,23 +116,26 @@ public class GameScreen extends ScreenAdapter {
             NPCsInitialized = true;
         }
 
-        // Update NPC dialogue manager
         int[] playerPos = player.getLocation();
         npcDialogueManager.update(delta, environmentManager.getNpcManager().getNpcOnMaps(), playerPos);
 
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
-
-        // Render game world
         mapRenderer.render(batch, camera, environmentManager, npcDialogueManager);
         renderPlayer();
         renderOtherItems();
         batch.end();
 
-        // Render weather effects on top of everything
         if (environmentManager.isWeatherActive()) {
             batch.begin();
             weatherRenderer.render(batch, delta, environmentManager);
+            batch.end();
+        }
+
+        if (messageTimer > 0) {
+            messageTimer -= delta;
+            batch.begin();
+            messageFont.draw(batch, currentMessage, 10, Gdx.graphics.getHeight() - 20);
             batch.end();
         }
 
@@ -154,34 +149,36 @@ public class GameScreen extends ScreenAdapter {
         boolean keyLeft = Gdx.input.isKeyPressed(Input.Keys.LEFT);
         boolean keyRight = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
 
-        // Time advancement cheats
         if (Gdx.input.isKeyJustPressed(Input.Keys.F1)) {
             myGame.time().cheatAdvanceTime(1);
         }
         if (Gdx.input.isKeyJustPressed(Input.Keys.F2)) {
             myGame.time().cheatAdvanceDate(1, myGame);
         }
-        // Money increase cheat
         if (Gdx.input.isKeyJustPressed(Input.Keys.F3)) {
             player.addMoney(1000);
         }
-
-        // Greenhouse interaction
         if (Gdx.input.isKeyJustPressed(Input.Keys.F4)) {
             handleGreenhouseInteraction();
         }
-
-        // Handle S key for dialogue
+        if (Gdx.input.isKeyJustPressed(Input.Keys.F5)) {
+            gameController.nextTurn();
+            currentMessage = "Next turn started!";
+            messageTimer = MESSAGE_DURATION;
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.F6)) {
+            gameController.exitGame();
+            currentMessage = "Game saved successfully!";
+            messageTimer = MESSAGE_DURATION;
+            game.setScreen(new MainMenuScreen(skin, game));
+        }
         if (Gdx.input.isKeyJustPressed(Input.Keys.S)) {
             handleNPCDialogue();
         }
-
-        // Open Menu
         if (Gdx.input.isKeyJustPressed(Input.Keys.M)) {
             player.setCurrentGameMenu("inventory");
         }
 
-        // Player movement
         if (keyUp || keyDown || keyLeft || keyRight) {
             moveTimer += delta;
             if (moveTimer < 0) {
@@ -200,17 +197,15 @@ public class GameScreen extends ScreenAdapter {
         int[] playerLocation = player.getLocation();
         int newX = playerLocation[0];
         int newY = playerLocation[1];
+
         if (keyUp) newY++;
         else if (keyDown) newY--;
         else if (keyLeft) newX--;
         else if (keyRight) newX++;
+
         if (newX >= 0 && newX < gameMap.getCurrentPlayerFarm(player).width() &&
                 newY >= 0 && newY < gameMap.getCurrentPlayerFarm(player).height()) {
             Entity entity = gameMap.getCurrentPlayerFarm(player).getEntityByLocation(newX, newY);
-            // Allow movement if:
-            // - No entity
-            // - Entity is WalkAble
-            // - Entity is a repaired greenhouse
             if (entity == null ||
                     entity instanceof WalkAble ||
                     (entity instanceof GreenHouse && gameController.greenhouse.isRepaired())) {
@@ -222,7 +217,7 @@ public class GameScreen extends ScreenAdapter {
     private void handleGreenhouseInteraction() {
         Space currentSpace = gameMap.getCurrentPlayerFarm(player);
         GreenHouse greenhouse = null;
-        // Search for greenhouse in player's current farm
+
         for (int x = 0; x < currentSpace.width(); x++) {
             for (int y = 0; y < currentSpace.height(); y++) {
                 Entity entity = currentSpace.getEntityByLocation(x, y);
@@ -233,6 +228,7 @@ public class GameScreen extends ScreenAdapter {
             }
             if (greenhouse != null) break;
         }
+
         if (greenhouse != null) {
             if (!greenhouse.isRepaired()) {
                 GreenhouseRepairDialog dialog = new GreenhouseRepairDialog(
@@ -243,16 +239,12 @@ public class GameScreen extends ScreenAdapter {
                         greenhouse.getRepairMoney()
                 );
                 dialog.show(stage);
-                // Add listener to refresh after dialog closes
                 dialog.addListener(new ChangeListener() {
                     @Override
                     public void changed(ChangeListener.ChangeEvent event, Actor actor) {
-                        // Force a refresh of the game state
-                        // This will ensure the greenhouse becomes walkable and texture changes
                         Gdx.app.postRunnable(new Runnable() {
                             @Override
                             public void run() {
-                                // Refresh the map renderer to update textures
                                 mapRenderer.dispose();
                                 mapRenderer = new MapRenderer(gameMap, player, TILE_SIZE, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
                             }
@@ -260,14 +252,12 @@ public class GameScreen extends ScreenAdapter {
                     }
                 });
             } else {
-                // Greenhouse is already repaired
                 Dialog infoDialog = new Dialog("", skin);
                 infoDialog.text("The greenhouse is already repaired and ready to use!");
                 infoDialog.button("OK");
                 infoDialog.show(stage);
             }
         } else {
-            // No greenhouse found in the farm
             Dialog infoDialog = new Dialog("", skin);
             infoDialog.text("No greenhouse found in your farm. You need to build one first!");
             infoDialog.button("OK");
@@ -278,36 +268,28 @@ public class GameScreen extends ScreenAdapter {
     private void handleNPCDialogue() {
         int[] playerPos = player.getLocation();
         Space currentSpace = gameMap.getCurrentPlayerFarm(player);
-
         int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
         for (int[] dir : directions) {
             int checkX = playerPos[0] + dir[0];
             int checkY = playerPos[1] + dir[1];
 
             if (checkX >= 0 && checkX < currentSpace.width() &&
                     checkY >= 0 && checkY < currentSpace.height()) {
-
                 Entity entity = currentSpace.getEntityByLocation(checkX, checkY);
                 if (entity instanceof NPCOnMap) {
                     NPCOnMap npcOnMap = (NPCOnMap) entity;
-
                     if (npcDialogueManager.isDialogueIconVisible(npcOnMap)) {
                         NPC npc = npcOnMap.getNpc();
-
                         String seasonStr = environmentManager.getCurrentSeason();
                         Season season = Season.valueOf(seasonStr.toUpperCase());
-
                         String dialogue = npc.getDialogueBySeason(season);
-
                         Dialog dialog = new Dialog(npc.getName(), skin);
                         dialog.text(dialogue);
                         dialog.button("OK");
                         dialog.show(stage);
-
                         npc.increaseFriendship(20);
-
                         npcDialogueManager.activateDialogue(npcOnMap);
-
                         break;
                     }
                 }
@@ -343,6 +325,7 @@ public class GameScreen extends ScreenAdapter {
         npcDialogueManager.dispose();
         stage.dispose();
         batch.dispose();
+        messageFont.dispose();
     }
 
     @Override
